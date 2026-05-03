c5857d4d278cc5cbf4a01ddd4043c576ffd150de
amira95ibrahim <amira95ibrahim@gmail.com>
2026-05-03 09:50:32 +0300

    initial code

---
+++ api/getInvoices.js
@@ -0,0 +1,36 @@
+export default async function handler(req, res) {
+    try {
+      const token = req.headers.authorization?.replace("Bearer ", "");
+  
+      if (!token) {
+        return res.status(401).json({ error: "No token" });
+      }
+  
+      let allInvoices = [];
+      let page = 1;
+  
+      while (true) {
+        const response = await fetch(
+          `https://api.invoicing.eta.gov.eg/api/v1/documents?pageSize=50&pageNo=${page}`,
+          {
+            headers: {
+              Authorization: "Bearer " + token,
+              "Content-Type": "application/json",
+            },
+          }
+        );
+  
+        const data = await response.json();
+  
+        if (!data.result || data.result.length === 0) break;
+  
+        allInvoices = allInvoices.concat(data.result);
+        page++;
+      }
+  
+      return res.status(200).json(allInvoices);
+    } catch (err) {
+      return res.status(500).json({ error: err.message });
+    }
+  }
+  
