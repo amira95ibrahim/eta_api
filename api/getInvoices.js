36e0dc7990b0231e4634f087e8e4d4d89a8a73d4
MeRoty <amira95ibrahim@gmail.com>
2026-05-09 19:54:04 +0300

    token

--- api/getInvoices.js
+++ api/getInvoices.js
@@ -61,7 +61,7 @@ export default async function handler(req, res) {
     const dataText = await response.text();
 
 
-    return res.status(200).send(response,token);
+    return res.status(200).send(token);
 
   } catch (err) {
 
